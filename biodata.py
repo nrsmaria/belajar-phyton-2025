68824d431da6b80ff6c64499fd8e754895a077d1
nrsmaria <marianursila3020@gmail.com>
2025-06-16 18:22:20 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,40 @@
+nama = input("Maria ")
+umur = input("25 ")
+hobi = input("Membaca ")
+
+biodata = {
+     "nama": nama,
+     "umur": umur,
+     "hobi": hobi 
+}
+
+# Menampilkan data secara rapi
+print("\n=== BIODATA SISWA ===")
+print("Nama : ", biodata["nama"])
+print("umur : ", biodata["umur"])
+print("hobi : ", biodata["hobi"])
+
+
+#input data
+nama = input("Masukkan nama: ")
+umur = input("Masukkan umur: ")
+hobi = input("Masukkan hobi: ")
+
+# simpan ke dictionary
+biodata = {
+    "Maria": nama,
+    "25": umur,
+    "Membaca": hobi
+}
+
+
+#Tanya user, mau lanjut input atau tidak
+lanjut = input("Mau tambah data lagi? (y/n: )")
+
+
+#Tampilkan semua data 
+print("\n=== DAFTAR BIODATA SISWA ===")
+
+    print("Nama :", siswa [Dina])
+    print("Nama :", siswa[21])
+    print("Hobi :", siswa[Bermain musik])
