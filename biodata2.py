80f558e13d22db1c993ca83c85401db5a768991e
nrsmaria <marianursila3020@gmail.com>
2025-06-16 18:37:18 +0700

    Add files via upload

---
+++ biodata2.py
@@ -0,0 +1,53 @@
+nama = input("Maria ")
+umur = input("25 ")
+hobi = input("Membaca ")
+
+biodata = {
+     "nama": nama,
+     "umur": umur,
+     "hobi": hobi 
+}
+
+# Menampilkan data secara rapi
+print("\n=== BIODATA SISWA ===")
+print("Nama : ", biodata["nama"])
+print("umur : ", biodata["umur"])
+print("hobi : ", biodata["hobi"])
+
+
+#input data
+nama = input("Masukkan nama: ")
+umur = input("Masukkan umur: ")
+hobi = input("Masukkan hobi: ")
+
+# simpan ke dictionary
+biodata = {
+    "Maria": nama,
+    "25": umur,
+    "Membaca": hobi
+}
+
+
+#Tanya user, mau lanjut input atau tidak
+lanjut = input("Mau tambah data lagi? (y/n: )")
+
+
+#Tampilkan semua data 
+print("\n=== DAFTAR BIODATA SISWA ===")
+
+    print("Nama :", siswa [Dina])
+    print("Nama :", siswa[21])
+    print("Hobi :", siswa[Bermain musik])
+
+siswa = {
+     "nama": "Maria Nursila"
+     "umur": "25"
+     "alamat" : "Bandung"
+     "email" : "maria@example.com"
+}
+
+print("=== Biodata Siswa ===")
+print(f"Nama : {siswa['Maria Nursila']}")
+print(f"Umur : {siswa['25']} tahun")
+print(f"alamat : {siswa['Bandung']}")
+print(f"Email : {siswa['email']}")
